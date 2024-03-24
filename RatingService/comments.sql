b64993614d1349f8c9f464d004ade6c3808ff0c2
cookie273 <stas.fedotov.1337@gmail.com>
2024-03-25 01:09:11 +0300

    add dumps files

---
+++ RatingService/comments.sql
@@ -0,0 +1,106 @@
+--
+-- PostgreSQL database dump
+--
+
+-- Dumped from database version 16.2
+-- Dumped by pg_dump version 16.2
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+SET default_tablespace = '';
+
+SET default_table_access_method = heap;
+
+--
+-- Name: comment; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.comment (
+    id integer NOT NULL,
+    username text,
+    restaurant text,
+    content text,
+    rating smallint
+);
+
+
+ALTER TABLE public.comment OWNER TO postgres;
+
+--
+-- Name: comment_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
+--
+
+CREATE SEQUENCE public.comment_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER SEQUENCE public.comment_id_seq OWNER TO postgres;
+
+--
+-- Name: comment_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
+--
+
+ALTER SEQUENCE public.comment_id_seq OWNED BY public.comment.id;
+
+
+--
+-- Name: comment id; Type: DEFAULT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.comment ALTER COLUMN id SET DEFAULT nextval('public.comment_id_seq'::regclass);
+
+
+--
+-- Data for Name: comment; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+COPY public.comment (id, username, restaurant, content, rating) FROM stdin;
+1	stas	The Rustic Table	Very good restaurant	5
+2	molodoy_na_volge	The Rustic Table	Very bad restaurant	1
+3	molodoy_na_volge	Thai Orchid Garden	Nice restaurant	4
+5	stas	Thai Orchid Garden	Nothing special	3
+6	stas	The Jopa	ќв® ­Ґ аҐбв®а ­, нв® ¦®Ї 	\N
+\.
+
+
+--
+-- Name: comment_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
+--
+
+SELECT pg_catalog.setval('public.comment_id_seq', 6, true);
+
+
+--
+-- Name: comment comment_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.comment
+    ADD CONSTRAINT comment_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: comment comment_username_restaurant_key; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.comment
+    ADD CONSTRAINT comment_username_restaurant_key UNIQUE (username, restaurant);
+
+
+--
+-- PostgreSQL database dump complete
+--
+
---
+++ RestaurantService/restaurants.sql
@@ -0,0 +1,133 @@
+--
+-- PostgreSQL database dump
+--
+
+-- Dumped from database version 16.2
+-- Dumped by pg_dump version 16.2
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+SET default_tablespace = '';
+
+SET default_table_access_method = heap;
+
+--
+-- Name: restaurant; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.restaurant (
+    id integer NOT NULL,
+    name text,
+    description text
+);
+
+
+ALTER TABLE public.restaurant OWNER TO postgres;
+
+--
+-- Name: restaurant_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
+--
+
+CREATE SEQUENCE public.restaurant_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER SEQUENCE public.restaurant_id_seq OWNER TO postgres;
+
+--
+-- Name: restaurant_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
+--
+
+ALTER SEQUENCE public.restaurant_id_seq OWNED BY public.restaurant.id;
+
+
+--
+-- Name: restaurant_keyword; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.restaurant_keyword (
+    restaurant_id integer NOT NULL,
+    keyword text NOT NULL
+);
+
+
+ALTER TABLE public.restaurant_keyword OWNER TO postgres;
+
+--
+-- Name: restaurant id; Type: DEFAULT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.restaurant ALTER COLUMN id SET DEFAULT nextval('public.restaurant_id_seq'::regclass);
+
+
+--
+-- Data for Name: restaurant; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+COPY public.restaurant (id, name, description) FROM stdin;
+1	Thai Orchid Garden	The best Thai restaurant
+2	The Rustic Table	We are so good
+\.
+
+
+--
+-- Data for Name: restaurant_keyword; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+COPY public.restaurant_keyword (restaurant_id, keyword) FROM stdin;
+1	meal
+1	thai
+2	Vegan
+2	no gluten
+\.
+
+
+--
+-- Name: restaurant_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
+--
+
+SELECT pg_catalog.setval('public.restaurant_id_seq', 2, true);
+
+
+--
+-- Name: restaurant_keyword restaurant_keyword_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.restaurant_keyword
+    ADD CONSTRAINT restaurant_keyword_pkey PRIMARY KEY (restaurant_id, keyword);
+
+
+--
+-- Name: restaurant restaurant_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.restaurant
+    ADD CONSTRAINT restaurant_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: restaurant_keyword restaurant_keyword_restaurant_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.restaurant_keyword
+    ADD CONSTRAINT restaurant_keyword_restaurant_id_fkey FOREIGN KEY (restaurant_id) REFERENCES public.restaurant(id);
+
+
+--
+-- PostgreSQL database dump complete
+--
+
